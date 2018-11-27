dec7ea0a6d04824ea772a475e6eaa19f1646f44d
Shawn Xu <xunnan.xu@gmail.com>
2018-11-26 23:00:38 -0800

    git debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,7 @@ set -exo pipefail
 if [[ ${TRAVIS_BRANCH} = release/* ]] && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
     openssl aes-256-cbc -K $encrypted_549eb73a0bc0_key -iv $encrypted_549eb73a0bc0_iv -in signing.asc.enc -out signing.asc -d
     ${GPG_EXECUTABLE} --fast-import signing.asc
-    mvn -B -U -P ossrh -DskipTests=true --settings .travis-settings.xml deploy gitflow:release-finish
+    mvn -B -U -P ossrh -DskipTests=true --settings .travis-settings.xml deploy
+    git status
+    mvn gitflow:release-finish
 fi
