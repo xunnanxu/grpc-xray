04a708d63a57bdf58988ccb17e5533219103cc14
Shawn Xu <xunnan.xu@gmail.com>
2018-11-26 22:48:07 -0800

    add signing config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -exo pipefail
 if [[ ${TRAVIS_BRANCH} = release/* ]] && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
-    openssl aes-256-cbc -K ${encrypted_39dc14e92501_key} -iv ${encrypted_39dc14e92501_iv} -in signing.asc.enc -d | ${GPG_EXECUTABLE} --fast-import
+    openssl aes-256-cbc -K $encrypted_549eb73a0bc0_key -iv $encrypted_549eb73a0bc0_iv -in signing.asc.enc -out signing.asc -d
+    ${GPG_EXECUTABLE} --fast-import signing.asc
     mvn -B -U -P ossrh -DskipTests=true --settings .travis-settings.xml deploy gitflow:release-finish
 fi
